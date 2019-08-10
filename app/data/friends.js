84013dc99bc33a900dcaec7dd381f273e0f88965
Tyler Nichols <tegulator98@gmail.com>
2019-08-10 11:25:57 -0600

    Data added to friends

---
+++ app/data/friends.js
@@ -0,0 +1,172 @@
+var data =[
+{
+    name:"Pikachu",
+    photo:"https://img.pokemondb.net/artwork/large/pikachu.jpg",
+    scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+},
+
+{
+
+    name: "Meowth",
+    photo: "https://img.pokemondb.net/artwork/large/meowth.jpg",
+    scores: [
+        2,
+        4,
+        5,
+        3,
+        1,
+        2,
+        3,
+        3,
+        4,
+        5
+    ]
+},
+{
+
+    name: "Oddish",
+    photo: "https://img.pokemondb.net/artwork/large/oddish.jpg",
+    scores: [
+        4,
+        5,
+        2,
+        1,
+        3,
+        1,
+        4,
+        2,
+        3,
+        3
+    ]
+},
+{
+
+    name: "Alakazam",
+    photo: "https://img.pokemondb.net/artwork/large/alakazam.jpg",
+    scores: [
+        5,
+        3,
+        3,
+        2,
+        4,
+        5,
+        1,
+        1,
+        3,
+        4
+    ]
+},
+{
+
+    name: "Garchomp",
+    photo: "https://img.pokemondb.net/artwork/large/garchomp.jpg",
+    scores: [
+        4,
+        3,
+        1,
+        4,
+        4,
+        3,
+        2,
+        2,
+        4,
+        3
+    ]
+},
+{
+
+    name: "Lucario",
+    photo: "https://img.pokemondb.net/artwork/large/lucario.jpg",
+    scores: [
+        3,
+        2,
+        2,
+        5,
+        5,
+        1,
+        4,
+        4,
+        3,
+        2
+    ]
+},
+{
+
+    name: "Mudsdale",
+    photo: "https://img.pokemondb.net/artwork/large/mudsdale.jpg",
+    scores: [
+        3,
+        2,
+        3,
+        4,
+        5,
+        1,
+        2,
+        2,
+        4,
+        1
+    ]
+},
+{
+
+    name: "Zoroark",
+    photo: "https://img.pokemondb.net/artwork/large/zoroark.jpg",
+    scores: [
+        1,
+        1,
+        4,
+        3,
+        5,
+        2,
+        2,
+        4,
+        5,
+        5
+    ]
+},
+{
+
+    name: "Luxray",
+    photo: "https://img.pokemondb.net/artwork/large/luxray.jpg",
+    scores: [
+        4,
+        2,
+        1,
+        3,
+        3,
+        4,
+        5,
+        5,
+        1,
+        1
+    ]
+},
+{
+
+    name: "Metagross",
+    photo: "https://img.pokemondb.net/artwork/large/metagross.jpg",
+    scores: [
+        2,
+        5,
+        4,
+        3,
+        2,
+        1,
+        2,
+        3,
+        3,
+        5
+    ]
+},
+]
--- server.js
+++ server.js
@@ -2,3 +2,8 @@ var express = require("express");
 var path = require("path");
 
 var app = express
+
+var PORT = 3030;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
