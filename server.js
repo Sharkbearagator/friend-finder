d1490c286378292902693dde8e331ef0b3fb52ad
Tyler Nichols <tegulator98@gmail.com>
2019-08-12 18:54:45 -0600

    initial commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 
 var app = express();
 
-var PORT = 3030;
+var PORT = process.env.PORT || 3030;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
